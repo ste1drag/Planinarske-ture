1934573e0e5115b2065ff37b098aafa6725dd5e4
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2025-02-26 20:03:16 +0100

    feat: Defined initial Domain logic

---
+++ backend/Services/Review/Review.Domain/ValueObjects/Score.cs
@@ -0,0 +1,30 @@
+﻿using Review.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.Domain.ValueObjects
+{
+    public class Score : ValueObjectBase
+    {
+        public int Value { get; }
+        private const int UPPER_BOUND = 5;
+        private const int LOWER_BOUND = 1;
+
+        public Score(int value)
+        {
+            if (value < LOWER_BOUND || value > UPPER_BOUND)
+            {
+                throw new ArgumentException($"Score must be between {LOWER_BOUND} and {UPPER_BOUND}");
+            }
+            Value = value;
+        }
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
