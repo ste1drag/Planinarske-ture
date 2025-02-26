4b4ee7328960e8535e6c3397854146e1d00f5373
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2025-02-26 20:03:16 +0100

    feat: Defined initial Domain logic

---
+++ backend/Services/Review/Review.Domain/Entities/Review.cs
@@ -0,0 +1,21 @@
+﻿using Review.Domain.Common;
+using Review.Domain.Enums;
+using Review.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.Domain.Entities
+{
+    public class Review(int userId, int tourId, string title, string? comment, Difficulty difficulty, Score score) : EntityBase
+    {
+        public int UserId { get; } = userId;
+        public int TourId { get; } = tourId;
+        public required string Title { get; set; } = title;
+        public string? Comment { get; set; } = comment;
+        public Difficulty Difficulty { get; set; } = difficulty;
+        public required Score Score { get; set; } = score;
+    }
+}
---
+++ backend/Services/Review/Review.Domain/Enums/Difficulty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.Domain.Enums
+{
+    public enum Difficulty
+    {
+        VERY_EASY = 1,
+        EASY = 2,
+        MEDIUM = 3,
+        HARD = 4,
+        VERY_HARD = 5
+    }
+}
