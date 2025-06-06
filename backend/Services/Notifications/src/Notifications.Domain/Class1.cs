8e2e490c6b55f1d5941a20c013f52ed92cdbe8d9
Uros Dimitrijevic <urosdim@icloud.com>
2025-06-06 21:02:23 +0200

    Initial project setup: Celan Architecture structure with  all project and references

---
+++ backend/Services/Notifications/src/Notifications.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Notifications.Domain;
+
+public class Class1
+{
+
+}
---
+++ backend/Services/Notifications/tests/Notifications.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Notifications.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
