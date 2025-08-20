6d6aea9d4d9e4acdaabd0257bb0bf29781a1efa7
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2025-08-21 00:03:58 +0200

    fix: tailwind downgrade

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./public/index.html"
-    ],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
     theme: {
       extend: {
         colors: {
