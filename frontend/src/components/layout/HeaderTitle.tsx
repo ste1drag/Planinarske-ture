35baa0c702b13893f25cb9d8dbdbb90ab0f128ef
VlajkovicPavle <vlajkovicpavle123@gmail.com>
2025-08-20 22:47:06 +0200

    feat: add header component

---
+++ frontend/src/components/layout/HeaderTitle.tsx
@@ -0,0 +1,21 @@
+interface HeaderTitleProps {
+  title: string;
+  subTitle?: string;
+  button?: React.ReactNode;
+}
+
+export default function HeaderTitle({
+  title,
+  subTitle,
+  button,
+}: HeaderTitleProps) {
+  return (
+    <div className="flex h-[10vh] bg-forest/10 justify-between p-4">
+      <div className="flex justify-center flex-col">
+        <h1 className="text-3xl">{title}</h1>
+        <h2 className="text-xl text-black/50">{subTitle}</h2>
+      </div>
+      <div className="flex items-center">{button}</div>
+    </div>
+  );
+}
