de56569e85182b01bf34b5a79d155767a594e75e
Uros Dimitrijevic <urosdim@icloud.com>
2025-10-10 17:55:09 +0200

    feat: rabbitMQ setup

--- backend/Services/Shared.Events/src/Shared.Events/Events/Tours/TourCreateEvent.cs
+++ backend/Services/Shared.Events/src/Shared.Events/Events/Tours/TourCreateEvent.cs
@@ -2,5 +2,15 @@ namespace Shared.Events.Events.Tours;
 
 public class TourCreateEvent
 {
-    
+    public record TourCreatedEvent
+    {
+        public Guid Id { get; init; }
+        public DateTime OccuredOn { get; init; }
+        
+        //DomainPayload
+        public string TourId { get; init; }
+        public string Title {get; init;}
+        public string CreateByUserId {get; init;}
+        
+    }
 }
